30117b28aa3e88dc4ea64f420116820607314d10
A4BHI <manthukafc@gmail.com>
2025-11-15 23:23:29 +0530

    Working with the website

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "hari sree genapathaye namah ",
-		})
+
+		ctx.HTML(200, "index.html", nil)
 	})
 
+	r.GET("/pfp", func(ctx *gin.Context) {
+		name := "Abhijith"
+		ctx.HTML(200, "pfp.html", gin.H{
+			"Data": name,
+		})
+	})
 	r.Run()
 }
